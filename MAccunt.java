b8af768160001de4c7978bea73a799b166dbfd82
Master-HH <111192536+Master-HH@users.noreply.github.com>
2022-08-13 21:06:57 +0430

    test

---
+++ MAccunt.java
@@ -0,0 +1,22 @@
+package kit.m_password;
+
+import framework.m_printer.CMDPrinter;
+import framework.m_printer.Printer;
+
+public class MAccunt {
+
+  public static void main(String[] args) {
+    CMDPrinter printer = new CMDPrinter();
+
+
+    start(printer);
+  }
+
+  public static void start(Printer printer){
+    printer.println("start "+ MAccunt.class.getSimpleName());
+    printer.println();
+    printer.println();
+
+
+  }
+}
